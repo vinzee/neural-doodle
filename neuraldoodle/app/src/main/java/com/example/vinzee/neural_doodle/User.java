bd282a0aa6a95df2c5d338b66524061a667c3de7
Ajinkya Tejankar <tejankarajinkya@gmail.com>
2018-05-09 23:40:41 -0400

    Integrate Prasad's code

--- neuraldoodle/app/src/main/java/com/example/vinzee/neural_doodle/User.java
+++ neuraldoodle/app/src/main/java/com/example/vinzee/neural_doodle/User.java
@@ -6,16 +6,20 @@ public class User {
     public String phone;
     public String address;
     public String userType;
+    public String userBio;
+    public String interests;
 
     public User(){
 
     }
 
-    public User(String name, String email, String phone, String address, String userType) {
+    public User(String name, String email, String phone, String address, String userType, String userBio, String interests) {
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.address = address;
         this.userType = userType;
+        this.userBio = userBio;
+        this.interests = interests;
     }
 }
