9ac353a53a01720857b492bd58c6001afeb934da
agrwlpriyank <agarwal4priyank@gmail.com>
2018-05-14 14:53:25 -0400

    HomeActivity for shared preference

--- neuraldoodle/app/src/main/java/com/example/vinzee/neural_doodle/HomeActivity.java
+++ neuraldoodle/app/src/main/java/com/example/vinzee/neural_doodle/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.vinzee.neural_doodle;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -9,6 +10,13 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,7 @@ public class HomeActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_home);
         pushFragment(new GalleryFragment());
+        this.getUser();
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -61,5 +70,44 @@ public class HomeActivity extends AppCompatActivity {
             transaction.replace(R.id.rootLayout, fragment);
             transaction.commit();
         }
+    }
+
+    public void getUser() {
+        final SharedPreferences pref;
+        pref = getSharedPreferences("user_details",MODE_PRIVATE);
+        FirebaseAuth auth;
+        DatabaseReference mFirebaseDatabase;
+        FirebaseDatabase mFirebaseInstance;
+        auth = FirebaseAuth.getInstance();
+        final String uID = auth.getCurrentUser().getUid();
+
+
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        // get reference to 'users' node
+        mFirebaseDatabase = mFirebaseInstance.getReference("users/" + uID);
+
+
+        mFirebaseDatabase.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get user value
+                        User userObj = dataSnapshot.getValue(User.class);
+                        SharedPreferences.Editor editor = pref.edit();
+                        editor.putString("uID",uID);
+                        editor.putString("name",userObj.name);
+                        editor.putString("email",userObj.email);
+                        editor.commit();
+
+
+                        //user.email now has your email value
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
     }
 }
